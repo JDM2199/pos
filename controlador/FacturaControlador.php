a1556ce7efa8b7f9f058a53a606fa8db017e1f56
[200~https://github.com/JDM2199/Sistema_Pos.git~ <juandanielmamani2199@gmail.com>
2024-09-15 03:52:42 -0400

    crud clientes

---
+++ controlador/FacturaControlador.php
@@ -0,0 +1,84 @@
+
+<?php
+$ruta = parse_url($_SERVER["REQUEST_URI"]);
+
+if (isset($ruta["query"])) {
+    if (
+        $ruta["query"] == "ctrRegFactura" ||
+        $ruta["query"] == "ctrEditFactura"||
+        $ruta["query"] == "ctrNumFactura"||
+        $ruta["query"] == "ctrUltimoCufd"||
+        $ruta["query"] == "ctrNuevoCufd"||
+        $ruta["query"] == "ctrEliFactura"){
+        $metodo = $ruta["query"];
+        $factura = new ControladorFactura();
+        $factura->$metodo();
+    }
+}
+
+class ControladorFactura
+{
+  
+    static public function ctrInfoFacturas()
+    {
+        $respuesta = ModeloFactura::mdlInfoFacturas();
+        return $respuesta;
+    }
+    static public function ctrRegFactura()
+    {
+        require "../modelo/facturaModelo.php";
+
+        $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+        $data = array(
+            "loginFactura" => $_POST["login"],
+            "password" => $password,
+            "perfil" => "Moderador"
+        );
+        $respuesta = ModeloFactura::mdlRegFactura($data);
+
+        echo $respuesta;
+    }
+    static public function ctrInfoFactura($id)
+    {
+        $respuesta = ModeloFactura::mdlInfoFactura($id);
+        return $respuesta;
+    }
+    static function ctrEliFactura()
+  {
+    require "../modelo/facturaModelo.php";
+    $id = $_POST["id"];
+
+    $respuesta = ModeloFactura::mdlEliFactura($id);
+    echo $respuesta;
+    }
+    static public function ctrNumFactura(){
+      require "../modelo/facturaModelo.php";
+
+      $respuesta= ModeloFactura::mdlNumFactura();
+
+      if ($respuesta["max(id_factura)"]==null) {
+        echo "1";
+      }else{
+        echo $respuesta["max(id_factura)"]+1;
+      }
+      
+    }
+    static public function ctrNuevoCufd(){
+
+      require "../modelo/facturaModelo.php";
+      
+      $data=array(
+        "cufd"=>$_POST["cufd"],
+        "fechaVigCufd"=>$_POST["fechaVigCufd"],
+        "codControlCufd"=>$_POST["codControlCufd"]
+      );
+    echo ModeloFactura::mdlNuevoCufd($data);
+    }
+    static public function ctrUltimoCufd(){
+      require "../modelo/facturaModelo.php";
+     
+      $respuesta=ModeloFactura::mdlUltimoCufd();
+      echo json_encode($respuesta);
+    }
+}
---
+++ modelo/facturaModelo.php
@@ -0,0 +1,91 @@
+<?php
+require_once "conexion.php";
+class ModeloFactura
+{
+    static public function mdlInfoFacturas()
+    {
+        $stmt = Conexion::Conectar()->prepare("select * from factura");
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+        $stmt->close();
+        $stmt->null;
+    }
+    static public function mdlRegFactura($data)
+    {
+        $loginFactura = $data["loginFactura"];
+        $password = $data["password"];
+        $perfil = $data["perfil"];
+
+        $stmt = Conexion::conectar()->prepare("insert into factura(login_factura, password, perfil) 
+        values('$loginFactura', '$password', '$perfil')");
+
+        if ($stmt->execute()) {
+            return "ok";
+        } else {
+            return "error";
+        }
+
+        $stmt->close();
+        $stmt->null();
+
+    }
+    static public function mdlInfoFactura($id)
+    {
+        $stmt = Conexion::conectar()->prepare("select * from factura where id_factura=$id");
+        $stmt->execute();
+
+        return $stmt->fetch();
+
+        $stmt->close();
+        $stmt->null;
+    }
+    static public function mdlEliFactura($id)
+    {
+        $stmt = Conexion::conectar()->prepare("delete from factura where id_factura=$id");
+
+        if ($stmt->execute()) {
+            return "ok";
+        } else {
+            return "error";
+        }
+
+        $stmt->close();
+        $stmt->null();
+    }
+    static public function mdlNumFactura(){
+        $stmt = Conexion::conectar()->prepare("select max(id_factura) from factura");
+        $stmt->execute();
+
+        return $stmt->fetch();
+
+        $stmt->close();
+        $stmt->null;
+    }
+    static public function mdlNuevoCufd($data){
+        $cufd=$data["cufd"];
+        $fechaVigCufd=$data["fechaVigCufd"];
+        $codControlCufd=$data["codControlCufd"]; 
+
+        $stmt = Conexion::conectar()->prepare("insert into cufd(codigo_cufd, codigo_control, fecha_vigencia) 
+        values('$cufd', '$codControlCufd', '$fechaVigCufd')");
+
+        if ($stmt->execute()) {
+            return "ok";
+        } else {
+            return "error";
+        }
+
+        $stmt->close();
+        $stmt->null();
+    }
+    static public function mdlUltimoCufd(){
+        $stmt = Conexion::conectar()->prepare("SELECT * FROM cufd WHERE id_cufd=(select max(id_cufd) from cufd)");
+        $stmt->execute();
+
+        return $stmt->fetch();
+
+        $stmt->close();
+        $stmt->null;
+    }
+}
